da24265dd78e62f72992d0725390a7b3918e42c0
coxy57 <158783816+1coxy57@users.noreply.github.com>
2025-03-06 11:09:09 -0500

    Update train_model.py

--- learning/train_model.py
+++ learning/train_model.py
@@ -1,10 +1,35 @@
-from .train import Trainer
+try:
+    from learning.train import *
+except:
+    from train import *
+import asyncio
 
-def main():
+class Log:
+    @staticmethod
+    def epoch_over(epoch: int, metrics: dict):
+        print(epoch,metrics)
+    
+    @staticmethod
+    def train_start() -> None:
+        print('train start')
+
+    @staticmethod
+    def epoch_end(results: dict):
+        print(results)
+
+
+async def main():
     t = Trainer()
-    res = t.train()
+    call = callback(
+        ep_end=Log.epoch_over,
+        train_start=Log.train_start,
+        train_end=Log.epoch_end,
+
+    )
+    t.add_callback(call)
+    res = await t.train(config="data.yaml")
     return res
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
